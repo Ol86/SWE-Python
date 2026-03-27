e82769d81c7e5152a3c86ca2ac6b37a05588dd55
Ol86 <ole.menke@gmail.com>
2026-03-27 08:10:38 +0100

    Update the format of the project and add some configs and extras

---
+++ src/library/config/dev_mode.py
@@ -0,0 +1,9 @@
+"""
+This module contains configuration values for the development mode, e.g. whether to use mock data or not.
+"""
+
+from typing import Final
+
+from library.config.config import app_config
+
+_dev_toml: Final = app_config.get("dev", {})
