11e63200a270d3bb43ff7a6653766c922fa8d227
Ol86 <ole.menke@gmail.com>
2026-04-01 09:41:25 +0200

    Add constructor to the member_sevice

--- src/library/service/member_service.py
+++ src/library/service/member_service.py
@@ -1,5 +1,7 @@
 """The bussiness logic for member read operations."""
 
+from library.repository.member_repository import MemberRepository
+
 __all__ = [
     "MemberService",
 ]
@@ -7,3 +9,7 @@ __all__ = [
 
 class MemberService:
     """The service class for member read operations."""
+
+    def __init__(self, repo: MemberRepository) -> None:
+        """Initialize the MemberService with a MemberRepository instance."""
+        self.repo: MemberRepository = repo
