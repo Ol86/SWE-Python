0c910f7cc11dc3bd878c7762fffc8ee36f3c244b
Paul Bräuninger <brpa1033@h-ka.de>
2026-04-04 15:28:01 +0200

    Add post method in write router

--- src/library/router/member_write_router.py
+++ src/library/router/member_write_router.py
@@ -1,11 +1,12 @@
 """The REST API router for member write operations."""
-
 from typing import Annotated, Final
 
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, Request, Response, status
 from loguru import logger
+from patient.router.dependencies import get_write_service
 
 from library.router.dependencies import get_member_write_service
+from library.router.member_creation_model import MemberCreationModel
 from library.service import MemberWriteService
 
 __all__ = [
@@ -13,6 +14,32 @@ __all__ = [
 ]
 
 member_write_router: Final = APIRouter(tags=["Write"])
+
+
+@member_write_router.post("")
+def post(
+    member_model: MemberCreationModel,
+    request: Request,
+    service: Annotated[MemberWriteService, Depends(get_write_service)]
+) -> Response:
+    """Create new member.
+
+    :param member_model: New member data.
+    :param request: Request object.
+    :param service: Write service object
+    :rtype: Response
+    :raises ValidationError: If pydantic validation fails.
+    :raises EmailExistsError: If email address already exists.
+    :raises UsernameExists: If username already exists.
+    """
+    logger.debug("member_model={}", member_model)
+    member_dto: Final = service.create(member_model.to_member())
+    logger.debug("member_dto={}", member_dto)
+
+    return Response(
+        status_code=status.HTTP_201_CREATED,
+        headers={"Location": f"{request.url}/{member_dto.id}"}
+    )
 
 
 @member_write_router.delete(
