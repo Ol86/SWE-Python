207e2ea7b4c1b086327fe8f52b4f4ab4e51c49ef
Paul Bräuninger <brpa1033@h-ka.de>
2026-04-06 19:04:17 +0200

    Fix wrong import

--- src/library/config/mail.py
+++ src/library/config/mail.py
@@ -2,7 +2,7 @@
 
 from typing import Final
 
-from patient.config.config import app_config
+from library.config.config import app_config
 
 __all__ = ["mail_enabled", "mail_host", "mail_port", "mail_timeout"]
 
