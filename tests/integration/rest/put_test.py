31c60dfa01ef9798a9f80fdafe703955f2baae06
Paul Bräuninger <brpa1033@h-ka.de>
2026-04-11 00:26:31 +0200

    Add put tests script with default put test

---
+++ tests/integration/rest/put_test.py
@@ -0,0 +1,46 @@
+"""Tests for PUT requests to the REST API."""
+
+from http import HTTPStatus
+from typing import Final
+
+from common_test import login
+from constants import CTX, REST_URL
+from httpx import put
+from pytest import mark
+
+
+@mark.rest
+@mark.put_request
+def test_update_member() -> None:
+    """Test for updating an existing member using REST API."""
+    #arrange
+    token: Final = login()
+    assert token is not None
+    member_id: Final = 40
+    if_match: Final = '"0"'
+    updated_member_data: Final = {
+        "username": "updated_member_rest",
+        "first_name": "REST",
+        "last_name": "Updated Member",
+        "gender": "M",
+        "date_of_birth": "1990-01-01",
+        "member_since": "2020-01-01",
+        "is_student": False,
+        "email_address": "updated_member@example.com"
+    }
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "If-Match": if_match,
+    }
+
+    #act
+    response: Final = put(
+        f"{REST_URL}/{member_id}",
+        json=updated_member_data,
+        headers=headers,
+        verify=CTX,
+    )
+
+    #assert
+    assert response.status_code == HTTPStatus.NO_CONTENT
+    assert not response.text
