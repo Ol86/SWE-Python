a2b94c341492bbaeaf46b91430c3d82bedcf9e72
Ol86 <ole.menke@gmail.com>
2026-04-12 21:44:12 +0200

    Add exception handler

--- src/library/fastapi_app.py
+++ src/library/fastapi_app.py
@@ -7,8 +7,9 @@ from contextlib import asynccontextmanager
 from time import time
 from typing import TYPE_CHECKING, Any, Final
 
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, status
 from fastapi.middleware.gzip import GZipMiddleware
+from library.problem_details import create_problem_details
 from loguru import logger
 from prometheus_fastapi_instrumentator import Instrumentator
 
@@ -21,7 +22,14 @@ from library.config.dev_mode import dev_db_populate, dev_keycloak_populate
 from library.repository.session_factory import engine
 from library.router import member_router, member_write_router, shutdown_router
 from library.security import router as auth_router
-from library.security import set_response_headers
+from library.security import AuthorizationError, LoginError, set_response_headers
+from library.service import (
+    EmailExistsError,
+    ForbiddenError,
+    NotFoundError,
+    UsernameExistsError,
+    VersionOutdatedError,
+)
 
 if TYPE_CHECKING:
     from collections.abc import AsyncGenerator, Awaitable, Callable
@@ -100,3 +108,77 @@ async def add_security_headers(request: Request, call_next: Callable[[Any], Awai
     response: Final[Response] = await call_next(request)
     set_response_headers(response)
     return response
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+# Exception Handling
+# ----------------------------------------------------------------------------------------------------------------------------------
+@app.add_exception_handler(NotFoundError)
+def not_found_exception_handler(_request: Request, _err: NotFoundError) -> Response:
+    """Exception handler for NotFoundError.
+
+    :param _request: The incoming request that caused the exception.
+    :param _err: The NotFoundError exception that was raised.
+    :return: A Response object with a 404 status code and the error message as plain.
+    """
+    return create_problem_details(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@app.add_exception_handler(ForbiddenError)
+def forbidden_exception_handler(_request: Request, _err: ForbiddenError) -> Response:
+    """Exception handler for ForbiddenError.
+
+    :param _request: The incoming request that caused the exception.
+    :param _err: The ForbiddenError exception that was raised.
+    :return: A Response object with a 403 status code and the error message as plain.
+    """
+    return create_problem_details(status_code=status.HTTP_403_FORBIDDEN)
+
+
+@app.add_exception_handler(AuthorizationError)
+def authorization_exception_handler(_request: Request, _err: AuthorizationError) -> Response:
+    """Exception handler for AuthorizationError.
+
+    :param _request: The incoming request that caused the exception.
+    :param _err: The AuthorizationError exception that was raised.
+    :return: A Response object with a 401 status code and the error message as plain.
+    """
+    return create_problem_details(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+@app.add_exception_handler(LoginError)
+def login_exception_handler(_request: Request, _err: LoginError) -> Response:
+    """Exception handler for LoginError.
+
+    :param _request: The incoming request that caused the exception.
+    :param _err: The LoginError exception that was raised.
+    :return: A Response object with a 401 status code and the error message as plain.
+    """
+    return create_problem_details(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+@app.add_exception_handler(EmailExistsError)
+def email_exists_exception_handler(_request: Request, err: EmailExistsError) -> Response:
+    """Exception handler for EmailExistsError.
+
+    :param _request: The incoming request that caused the exception.
+    :param err: The EmailExistsError exception that was raised.
+    :return: A Response object with a 409 status code and the error message as plain.
+    """
+    return create_problem_details(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=str(err),
+    )
+
+
+@app.add_exception_handler(UsernameExistsError)
+def username_exists_exception_handler(_request: Request, err: UsernameExistsError) -> Response:
+    """Exception handler for UsernameExistsError.
+
+    :param _request: The incoming request that caused the exception.
+    :param err: The UsernameExistsError exception that was raised.
+    :return: A Response object with a 409 status code and the error message as plain.
+    """
+    return create_problem_details(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=str(err),
+    )
