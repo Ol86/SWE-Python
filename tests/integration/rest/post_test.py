a86bcda4e53afd52d2a2b216c928444ca9365bf6
Paul Bräuninger <brpa1033@h-ka.de>
2026-04-13 12:04:15 +0200

    Add post test with invalid test data

--- tests/integration/rest/post_test.py
+++ tests/integration/rest/post_test.py
@@ -1,5 +1,4 @@
 """Tests for POST requests to the REST API."""
-
 from http import HTTPStatus
 from re import search
 from typing import Final
@@ -57,3 +56,52 @@ def test_create_member() -> None:
     int_pattern: Final = "[1-9][0-9]*$"
     assert search(int_pattern, location_header) is not None
     assert not response.text
+
+
+@mark.rest
+@mark.post_request
+def test_create_member_with_invalid_data() -> None:
+    """Test for creating a new member with invalid data using REST API."""
+    #arrange
+    invalid_member_data: Final = {
+        "username": "invalid_member_rest",
+        "first_name": "Invalid",
+        "last_name": "last name",
+        "gender": "X",
+        "date_of_birth": "1990-01-01",
+        "member_since": "2020-01-01",
+        "is_student": False,
+        "email_address": "invalid_member.example",
+        "interests": ["F"],
+        "address": {
+            "postal_code": "5",
+            "place": "Test City",
+        },
+        "books": [
+            {
+                "name": "Invalid REST Testing",
+                "isbn": "978-3-16-148410-0",
+                "author": "John Doe",
+                "still_borrowed": True,
+                "genre": "F",
+            }
+        ],
+    }
+    headers = {"Content-Type": "application/json"}
+
+    #act
+    response: Final = post(
+        REST_URL,
+        json=invalid_member_data,
+        headers=headers,
+        verify=CTX,
+    )
+
+    #assert
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+    response_body = response.text
+    assert response_body is not None
+    assert "last_name" in response_body
+    assert "gender" in response_body
+    assert "email_address" in response_body
+    assert "postal_code" in response_body
